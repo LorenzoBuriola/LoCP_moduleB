885c3728fee1ac6937b3943947132287a81e9206
Confizolo <filippo.conforto@outlook.com>
2021-04-03 20:55:22 +0200

    Updates

--- Week5/RBM.py
+++ Week5/RBM.py
@@ -15,12 +15,14 @@ class RBM(object):
     w, a, b
     """
 
-    def __init__(self, l_rate = 1, nepochs = 50, mini = 500, M = 3, beta = 1, random_state = None):
+    def __init__(self, l_rate = 1, nepochs = 50, mini = 500, M = 3, beta = 1, beta_h=1, vmin = -1, random_state = None):
         self.l_rate = l_rate
         self.nepochs = nepochs
         self.mini = mini
         self.M = M
         self.beta = beta
+        self.beta_h = beta_h
+        self.vmin = vmin
         if random_state:
             np.random.seed(random_state)
     
@@ -31,7 +33,7 @@ class RBM(object):
         self._initialize_weights(self.L, self.M)
         GAP = v[0].max() - v[0].min()
         m = 0
-        for epoch in range(1, 1+self.nepoch):
+        for epoch in range(1, 1+self.nepochs):
             for n in range(N):
                 if m==0:
                     #initialize
@@ -40,11 +42,11 @@ class RBM(object):
                     vh_data, vh_model = np.zeros((self.L,self.M)), np.zeros((self.L, self.M))
 
                 #positive CD phase
-                h = self.activate(v[n], self.w, self.b, GAP)
+                h = self.activate(v[n], self.w, self.b, GAP, self.beta_h)
                 #negative CD phase
-                vf = self.activate(h, self.w.T, self.a, GAP)
+                vf = self.activate(h, self.w.T, self.a, GAP, self.beta)
                 # positive CD pahse nr 2
-                hf = self.activate(vf, self.w, self.b, GAP)
+                hf = self.activate(vf, self.w, self.b, GAP, self.beta_h)
 
                 v_data += v[n]
                 v_model += vf
@@ -70,10 +72,10 @@ class RBM(object):
         L = v.shape[1]
         N = v.shape[0]
         GAP = v[0].max() - v[0].min()
-        v1 = np.full((N, L), v.min())
+        v1 = np.full((N, L), self.vmin)
         for n in range(N):
-            h = self.activate(v[n],self.w, self.b, GAP)
-            v1[n] = self.activate(h, self.w.T, self.a, GAP)
+            h = self.activate(v[n],self.w, self.b, GAP, self.beta_h)
+            v1[n] = self.activate(h, self.w.T, self.a, GAP, self.beta)
         count = 0
         for n in range(N):
             for i in range(len(seq)):
@@ -100,11 +102,13 @@ class RBM(object):
         db = C*(h_data-h_model)
         self.b += db
 
-    def activate(self, v_in, wei, bias, DE):
+    def activate(self, v_in, wei, bias, DE, beta):
         act = np.dot(v_in, wei) + bias
-        prob = 1./(1.+np.exp(-self.beta*DE*act))
+        prob = 1./(1.+np.exp(-beta*DE*act))
         n = len(act)
-        v_out = np.full(n, v_in.min())
+        v_out = np.full(n, self.vmin)
         v_out[np.random.random_sample(n) < prob] = 1
         return v_out
-
+    
+    def params(self):
+        return self.w,self.a,self.b
