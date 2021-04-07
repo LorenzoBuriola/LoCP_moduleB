430b3afd13f4284761f31c85d39b9c512a9dd080
Filippo <filippo-xps137390@pop-os.localdomain>
2021-04-07 18:22:59 +0200

    Update

--- Week5/RBM.py
+++ Week5/RBM.py
@@ -15,14 +15,11 @@ class RBM(object):
     w, a, b
     """
 
-    def __init__(self, l_rate = 1, nepochs = 50, mini = 500, M = 3, beta = 1, beta_h=1, vmin = -1, random_state = None):
+    def __init__(self, l_rate = 1, nepochs = 50, mini = 500, M = 3, random_state = None):
         self.l_rate = l_rate
         self.nepochs = nepochs
         self.mini = mini
         self.M = M
-        self.beta = beta
-        self.beta_h = beta_h
-        self.vmin = vmin
         self.dw_history = []
         if random_state:
             np.random.seed(random_state)
@@ -43,11 +40,11 @@ class RBM(object):
                     vh_data, vh_model = np.zeros((self.L,self.M)), np.zeros((self.L, self.M))
 
                 #positive CD phase
-                h = self.activate(v[n], self.w, self.b, GAP, self.beta_h)
+                h = self.activate(v[n], self.w, self.b, GAP)
                 #negative CD phase
-                vf = self.activate(h, self.w.T, self.a, GAP, self.beta)
+                vf = self.activate(h, self.w.T, self.a, GAP)
                 # positive CD pahse nr 2
-                hf = self.activate(vf, self.w, self.b, GAP, self.beta_h)
+                hf = self.activate(vf, self.w, self.b, GAP)
 
                 v_data += v[n]
                 v_model += vf
@@ -69,20 +66,36 @@ class RBM(object):
             self.l_rate = self.l_rate/(0.05*self.l_rate + 1)
         return self
 
-    def score(self, v, seq):
+    def score(self, v, seq, beta = 1, beta_h=1):
         L = v.shape[1]
         N = v.shape[0]
         GAP = v[0].max() - v[0].min()
-        v1 = np.full((N, L), self.vmin)
+        vmin = -GAP +1
+        v1 = np.full((N, L), vmin)
         for n in range(N):
-            h = self.activate(v[n],self.w, self.b, GAP, self.beta_h)
-            v1[n] = self.activate(h, self.w.T, self.a, GAP, self.beta)
+            h = self.activate(v[n],self.w, self.b, GAP, beta_h)
+            v1[n] = self.activate(h, self.w.T, self.a, GAP, beta)
         count = 0
         for n in range(N):
             for i in range(len(seq)):
                 if (v1[n] == seq[i]).all():
                     count += 1
                     break
+        return count/N
+    
+    def compare(self, v, v_c, beta = 1, beta_h=1):
+        L = v.shape[1]
+        N = v.shape[0]
+        GAP = v[0].max() - v[0].min()
+        vmin = -GAP +1
+        v1 = np.full((N, L), vmin)
+        for n in range(N):
+            h = self.activate(v[n],self.w, self.b, GAP, beta_h)
+            v1[n] = self.activate(h, self.w.T, self.a, GAP, beta)
+        count = 0
+        for n in range(N):
+            if (v1[n] == v_c[n]).all():
+                count += 1
         return count/N
 
     def _initialize_weights(self, L, M):
@@ -104,11 +117,12 @@ class RBM(object):
         db = C*(h_data-h_model)
         self.b += db
 
-    def activate(self, v_in, wei, bias, DE, beta):
+    def activate(self, v_in, wei, bias, DE, beta = 1):
+        vmin = -DE +1
         act = np.dot(v_in, wei) + bias
         prob = 1./(1.+np.exp(-beta*DE*act))
         n = len(act)
-        v_out = np.full(n, self.vmin)
+        v_out = np.full(n, vmin)
         v_out[np.random.random_sample(n) < prob] = 1
         return v_out
     
